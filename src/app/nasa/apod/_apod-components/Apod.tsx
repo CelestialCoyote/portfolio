923ffd62b5b8739fb3ffd79b07236d825ff13123
Paul Stearns <paulstearns@Pauls-MacBook-Pro.local>
2025-02-02 17:39:49 -0800

    datepicker styling

--- src/app/nasa/apod/_apod-components/Apod.tsx
+++ src/app/nasa/apod/_apod-components/Apod.tsx
@@ -2,6 +2,8 @@
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { FaRegCalendarDays } from "react-icons/fa6";
+import { MdKeyboardArrowLeft, MdKeyboardDoubleArrowLeft, MdKeyboardArrowRight, MdKeyboardDoubleArrowRight } from "react-icons/md";
 import ApodSkeleton from "./ApodSkeleton";
 import { getApodData } from "../_apod-utils/getApodData";
 import { ApodData } from "../types/apodTypes";
@@ -14,6 +16,7 @@ import "./date-picker.css";
 const Apod: React.FC = () => {
     const [apod, setApod] = useState<ApodData | null>(null);
     const [error, setError] = useState<string | null>(null);
+    const [showCalendar, setShowCalendar] = useState(false);
     const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
     useEffect(() => {
@@ -38,6 +41,7 @@ const Apod: React.FC = () => {
 
     const handleFetchForDate = () => {
         if (selectedDate) {
+            console.log(selectedDate)
             const formattedDate = selectedDate.toISOString().split("T")[0];
 
             fetchApod(formattedDate);
@@ -46,28 +50,107 @@ const Apod: React.FC = () => {
 
     return (
         <div className="p-4">
-            <h1 className="text-3xl text-center mt-4 mb-6">
+            <h1 className="text-3xl text-center mb-6">
                 Astronomy Picture of the Day
             </h1>
 
-            {/* Date Picker */}
-            <div className="flex justify-center mb-2 space-x-2">
-                <DatePicker
-                    // className="bg-black text-purple-500 border-2 border-purple-500 rounded-lg px-3 py-2 w-[150px]"
-                    className="custom-datepicker"
-                    dateFormat="yyyy-MM-dd"
-                    placeholderText="Select Date"
-                    selected={selectedDate}
-                    onChange={handleDateChange}
-                    showIcon
-                />
-
+            <div className="flex flex-col items-center h-[100px] mt-2">
                 <button
-                    className="bg-black text-white font-bold text-center w-[100px] border-purple-500 border-2 rounded-lg px-2 py-2 hover:bg-purple-700"
-                    onClick={handleFetchForDate}
+                    className="flex justify-center items-center bg-black text-white font-bold w-[170px] gap-x-2 border-purple-500 border-2 rounded-lg p-2 hover:bg-purple-700"
+                    onClick={() => setShowCalendar(prev => !prev)}
                 >
-                    Get Image
+                    {showCalendar ? "Hide Calendar" : "Show Calendar"}
+                    {!showCalendar && <FaRegCalendarDays className="text-purple-500 w-[20px] h-[20px]" />}
                 </button>
+
+                {showCalendar &&
+                    <div className="flex justify-center items-center mt-2 mb-2 space-x-2">
+                        <div className="flex justify-center w-[225px]">
+                            {/* <DatePicker
+                                className="custom-datepicker"
+                                minDate={new Date("1995-06-20")}
+                                maxDate={new Date()}
+                                isClearable
+                                showYearDropdown
+                                dateFormat="yyyy-MM-dd"
+                                placeholderText="Choose a date"
+                                selected={selectedDate}
+                                onChange={handleDateChange}
+                                
+                            /> */}
+                            <DatePicker
+                                className="custom-datepicker"
+                                minDate={new Date("1995-06-20")}
+                                maxDate={new Date()}
+                                isClearable
+                                showYearDropdown
+                                dateFormat="yyyy-MM-dd"
+                                placeholderText="Choose a date"
+                                selected={selectedDate}
+                                onChange={handleDateChange}
+                                renderCustomHeader={({
+                                    date,
+                                    decreaseMonth,
+                                    increaseMonth,
+                                    prevMonthButtonDisabled,
+                                    nextMonthButtonDisabled,
+                                    decreaseYear,
+                                    increaseYear
+                                }) => (
+                                    <div className="flex justify-between items-center bg-purple-400 text-black px-4 py-2">
+                                        {/* << Button (Previous Year) */}
+                                        <button
+                                            onClick={decreaseYear}
+                                            disabled={prevMonthButtonDisabled}
+                                            className="text-2xl font-bold"
+                                        >
+                                            <MdKeyboardDoubleArrowLeft />
+                                        </button>
+
+                                        {/* < Button (Previous Month) */}
+                                        <button
+                                            onClick={decreaseMonth}
+                                            disabled={prevMonthButtonDisabled}
+                                            className="text-2xl font-bold"
+                                        >
+                                            <MdKeyboardArrowLeft />
+                                        </button>
+
+                                        {/* Current Month & Year */}
+                                        <span className="font-semibold">
+                                            {date.toLocaleString("default", { month: "long" })} {date.getFullYear()}
+                                        </span>
+
+                                        {/* > Button (Next Month) */}
+                                        <button
+                                            onClick={increaseMonth}
+                                            disabled={nextMonthButtonDisabled}
+                                            className="text-2xl font-bold"
+                                        >
+                                            <MdKeyboardArrowRight />
+                                        </button>
+
+                                        {/* >> Button (Next Year) */}
+                                        <button
+                                            onClick={increaseYear}
+                                            disabled={nextMonthButtonDisabled}
+                                            className="text-2xl font-bold"
+                                        >
+                                            <MdKeyboardDoubleArrowRight />
+                                        </button>
+                                    </div>
+                                )}
+                            />
+                        </div>
+
+                        <button
+                            className="bg-black text-white font-bold text-center w-[100px] border-purple-500 border-2 rounded-[10px] px-2 py-2 hover:bg-purple-700"
+                            onClick={handleFetchForDate}
+                        >
+                            Get Image
+                        </button>
+                    </div>
+                }
             </div>
 
             {error && <p className="text-red-500 text-center">{error}</p>}
@@ -104,7 +187,7 @@ const Apod: React.FC = () => {
                         <h2 className="text-2xl font-semibold text-gray-200">
                             {apod.title}
                         </h2>
-                        
+
                         {apod.copyright && (
                             <p className="text-sm text-gray-400">
                                 {`Credit: ${apod.copyright}`}
