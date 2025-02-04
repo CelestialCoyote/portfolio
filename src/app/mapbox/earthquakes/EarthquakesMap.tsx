837e7673ad79bb573599b1e672b2b30dfcd34ec0
Paul Stearns <paulstearns@Pauls-MacBook-Pro.local>
2025-02-03 18:59:06 -0800

    work on earthquake map

--- src/app/mapbox/earthquakes/EarthquakesMap.tsx
+++ src/app/mapbox/earthquakes/EarthquakesMap.tsx
@@ -4,10 +4,13 @@ import React, { useEffect, useMemo, useState, useRef } from "react";
 import Map, { Layer, MapRef, Marker, Popup, Source } from "react-map-gl";
 import { GeolocateControl, NavigationControl, ScaleControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import "./map-popup.module.css";
 
-import TectonicPlates from "./_earthquake-components/TectonicPlateBoundaries.json";
+// import TectonicPlates from "./_earthquake-components/TectonicPlateBoundaries.json";
+import PlateBoundaries from "./_earthquake-components/Tectonic_Plates.json";
 import ControlPanel from "./_earthquake-components/control-panel";
 import Skeleton from "../map-skeleton";
+
 
 const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
@@ -27,7 +30,7 @@ const initialViewState: InitialViewState = {
     zoom: 3.5,
     minZoom: 2.5,
     maxZoom: 18,
-};
+}
 
 interface Earthquake {
     id: string;
@@ -65,7 +68,7 @@ const filterFeaturesByDay = (
     });
 
     return { type: "FeatureCollection", features: filteredFeatures };
-};
+}
 
 const EarthquakesMap: React.FC = () => {
     const mapRef = useRef<MapRef>(null);
@@ -151,7 +154,18 @@ const EarthquakesMap: React.FC = () => {
                 style={{ borderRadius: 8 }}
                 onLoad={() => setLoading(false)}
             >
-                <Source id="tectonic-plates-source" type="geojson" data={TectonicPlates}>
+                {/* <Source id="tectonic-plates-source" type="geojson" data={TectonicPlates}>
+                    <Layer
+                        id="tectonic-plates-layer"
+                        type="line"
+                        paint={{
+                            "line-color": "#FF5733",
+                            "line-width": 2,
+                        }}
+                    />
+                </Source> */}
+
+                <Source id="tectonic-plates-source" type="geojson" data={PlateBoundaries}>
                     <Layer
                         id="tectonic-plates-layer"
                         type="line"
@@ -190,7 +204,7 @@ const EarthquakesMap: React.FC = () => {
                         onClose={() => setSelectedEarthquake(null)}
                         closeOnClick={false}
                     >
-                        <div>
+                        <div className="bg-slate-400 text-black p-1">
                             <h3>{selectedEarthquake.properties.place}</h3>
                             <p>Magnitude: {selectedEarthquake.properties.mag}</p>
                             <p>Depth: {selectedEarthquake.geometry.coordinates[2]} km</p>
@@ -201,401 +215,20 @@ const EarthquakesMap: React.FC = () => {
                 <ScaleControl position="bottom-right" />
                 <GeolocateControl position="bottom-right" />
                 <NavigationControl position="bottom-right" />
-            </Map>
 
-            <ControlPanel
-                startTime={timeRange[0]}
-                endTime={timeRange[1]}
-                selectedTime={selectedTime}
-                allDays={allDays}
-                onChangeTime={setSelectedTime}
-                onChangeAllDays={setAllDays}
-            />
+                <div className="absolute top-4 right-4">
+                    <ControlPanel
+                        startTime={timeRange[0]}
+                        endTime={timeRange[1]}
+                        selectedTime={selectedTime}
+                        allDays={allDays}
+                        onChangeTime={setSelectedTime}
+                        onChangeAllDays={setAllDays}
+                    />
+                </div>
+            </Map>
         </div>
     );
-};
+}
 
 export default EarthquakesMap;
-
-
-
-
-
-
-// import React, { useEffect, useMemo, useState, useRef } from "react";
-// import Map, { Layer, LayerProps, MapRef, Marker, Popup, Source } from "react-map-gl";
-// import { GeolocateControl, NavigationControl, ScaleControl } from "react-map-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
-
-// import TectonicPlates from "./_earthquake-components/TectonicPlateBoundaries.json";
-// import ControlPanel from "./_earthquake-components/control-panel";
-// import Skeleton from "../map-skeleton";
-// import ZoomLevelDisplay from "../_mapbox-components/ZoomLevelDisplay";
-
-// const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
-
-// interface InitialViewState {
-//     longitude: number;
-//     latitude: number;
-//     projection: string;
-//     zoom: number;
-//     minZoom: number;
-//     maxZoom: number;
-// }
-
-// const initialViewState: InitialViewState = {
-//     longitude: -98.583333,
-//     latitude: 39.833333,
-//     projection: "globe",
-//     zoom: 3.5,
-//     minZoom: 2.5,
-//     maxZoom: 18
-// };
-
-// interface Earthquake {
-//     id: string;
-//     properties: {
-//         mag: number;
-//         place: string;
-//     };
-//     geometry: {
-//         coordinates: [number, number, number]; // [longitude, latitude, depth]
-//     };
-// }
-
-// const TectonicPlatesLayer: LayerProps = {
-//     id: "tectonic-plates-layer",
-//     type: "line",
-//     paint: {
-//         "line-color": "#FF5733", // Orange-red color
-//         "line-width": 2,        // Adjust thickness
-//     },
-// };
-
-// const filterFeaturesByDay = (featureCollection, time) => {
-//     const date = new Date(time);
-//     const year = date.getFullYear();
-//     const month = date.getMonth();
-//     const day = date.getDate();
-//     const features = featureCollection.features.filter(feature => {
-//         const featureDate = new Date(feature.properties.time);
-
-//         return (
-//             featureDate.getFullYear() === year &&
-//             featureDate.getMonth() === month &&
-//             featureDate.getDate() === day
-//         );
-//     });
-
-//     return { type: 'FeatureCollection', features };
-// };
-
-// const EarthquakesMap: React.FC = () => {
-//     const mapRef = useRef<MapRef>(null);
-//     const [loading, setLoading] = useState(true);
-//     const [earthquakes, setEarthquakes] = useState<Earthquake[]>([]);
-//     const [selectedEarthquake, setSelectedEarthquake] = useState<Earthquake | null>(null);
-
-//     const [allDays, useAllDays] = useState(true);
-//     const [timeRange, setTimeRange] = useState([0, 0]);
-//     const [selectedTime, selectTime] = useState(0);
-
-
-//     const fetchEarthquakeData = async () => {
-//         const now = new Date();
-//         const thirtyDaysAgo = new Date();
-//         thirtyDaysAgo.setDate(now.getDate() - 30);
-
-//         const startTime = thirtyDaysAgo.toISOString();
-//         const endTime = now.toISOString();
-
-//         const url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${startTime}&endtime=${endTime}&minmagnitude=2.5`;
-
-//         const response = await fetch(url);
-
-//         if (!response.ok) {
-//             throw new Error("Failed to fetch earthquake data");
-//         }
-
-//         return response.json();
-//     };
-
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             try {
-//                 const data = await fetchEarthquakeData();
-//                 setEarthquakes(data.features);
-//             } catch (error) {
-//                 console.error("Error fetching earthquake data:", error);
-//             }
-//         };
-
-//         fetchData();
-//     }, []);
-
-//     if (!mapboxToken) {
-//         console.error("Mapbox token is missing. Check your environment variables.");
-//         return (
-//             <div className="text-red-500 text-center mt-10">
-//                 Error: Mapbox token not found.
-//             </div>
-//         );
-//     }
-
-//     const getColorByMagnitude = (mag: number) => {
-//         const colors = [
-//             { mag: 2.0, color: "green" },
-//             { mag: 3.0, color: "#88C057" }, // light green
-//             { mag: 4.0, color: "yellow" },
-//             { mag: 5.0, color: "#FFC100" }, // orange-yellow
-//             { mag: 6.0, color: "orange" },
-//             { mag: 7.0, color: "#FF4500" }, // red-orange
-//             { mag: 8.0, color: "red" },
-//             { mag: 9.0, color: "#B22222" }, // dark red
-//         ];
-
-//         for (let i = colors.length - 1; i >= 0; i--) {
-//             if (mag >= colors[i].mag) {
-//                 return colors[i].color;
-//             }
-//         }
-//         return "green"; // Default for magnitudes < 2.0
-//     };
-
-//     const data = useMemo(() => {
-//         return allDays ? earthquakes : filterFeaturesByDay(earthquakes, selectedTime);
-//     }, [earthquakes, allDays, selectedTime]);
-
-//     return (
-//         <div className="flex flex-row w-full h-full max-h-full gap-x-4">
-//             {loading && <Skeleton message="Loading earthquakes map..." />}
-
-//             <Map
-//                 initialViewState={initialViewState}
-//                 mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
-//                 mapboxAccessToken={mapboxToken}
-//                 ref={mapRef}
-//                 style={{ borderRadius: 8 }}
-//                 onLoad={() => setLoading(false)}
-//             >
-//                 {/* Tectonic Plates GeoJSON Layer */}
-//                 <Source id="tectonic-plates-source" type="geojson" data={TectonicPlates}>
-//                     <Layer {...TectonicPlatesLayer} />
-//                 </Source>
-
-//                 {data && earthquakes.map((quake) => (
-//                     <Marker
-//                         key={quake.id}
-//                         longitude={quake.geometry.coordinates[0]}
-//                         latitude={quake.geometry.coordinates[1]}
-//                         onClick={() => setSelectedEarthquake(quake)}
-//                     >
-//                         <div
-//                             style={{
-//                                 width: "10px",
-//                                 height: "10px",
-//                                 backgroundColor: getColorByMagnitude(quake.properties.mag),
-//                                 borderRadius: "50%",
-//                                 border: "1px solid white",
-//                                 cursor: "pointer",
-//                             }}
-//                         ></div>
-//                     </Marker>
-//                 ))}
-
-//                 {selectedEarthquake && (
-//                     <Popup
-//                         longitude={selectedEarthquake.geometry.coordinates[0]}
-//                         latitude={selectedEarthquake.geometry.coordinates[1]}
-//                         onClose={() => setSelectedEarthquake(null)}
-//                         closeOnClick={false}
-//                     >
-//                         <div>
-//                             <h3>{selectedEarthquake.properties.place}</h3>
-//                             <p>Magnitude: {selectedEarthquake.properties.mag}</p>
-//                             <p>Depth: {selectedEarthquake.geometry.coordinates[2]} km</p>
-//                         </div>
-//                     </Popup>
-//                 )}
-
-//                 <ScaleControl position="bottom-right" />
-//                 <GeolocateControl position="bottom-right" />
-//                 <NavigationControl position="bottom-right" />
-
-//                 <div className="absolute top-2 left-0 right-0 hidden lg:block">
-//                     <ZoomLevelDisplay
-//                         mapRef={mapRef}
-//                         initialViewState={initialViewState}
-//                     />
-//                 </div>
-
-//                 <div className="absolute top-2 right-2 hidden lg:block">
-//                     <ControlPanel
-//                         startTime={timeRange[0]}
-//                         endTime={timeRange[1]}
-//                         selectedTime={selectedTime}
-//                         allDays={allDays}
-//                         onChangeTime={selectTime}
-//                         onChangeAllDays={useAllDays}
-//                     />
-//                 </div>
-//             </Map>
-//         </div>
-//     );
-// };
-
-// export default EarthquakesMap;
-
-
-
-
-
-
-// import React, { useState, useEffect, useRef } from "react";
-// import Map, { MapRef, Marker, Popup } from "react-map-gl";
-// import { GeolocateControl, NavigationControl, ScaleControl } from "react-map-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
-
-// import Skeleton from "../map-skeleton";
-// import ZoomLevelDisplay from "../_mapbox-components/ZoomLevelDisplay";
-
-
-// const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
-
-// interface InitialViewState {
-//     longitude: number;
-//     latitude: number;
-//     projection: string;
-//     zoom: number;
-//     minZoom: number;
-//     maxZoom: number;
-// }
-
-// const initialViewState: InitialViewState = {
-//     longitude: -98.583333,
-//     latitude: 39.833333,
-//     projection: "globe",
-//     zoom: 3.5,
-//     minZoom: 2.5,
-//     maxZoom: 18
-// };
-
-// interface Earthquake {
-//     id: string;
-//     properties: {
-//         mag: number;
-//         place: string;
-//     };
-//     geometry: {
-//         coordinates: [number, number, number]; // [longitude, latitude, depth]
-//     };
-// }
-
-// const EarthquakesMap: React.FC = () => {
-//     const mapRef = useRef<MapRef>(null);
-//     const [loading, setLoading] = useState(true);
-//     const [earthquakes, setEarthquakes] = useState<Earthquake[]>([]);
-//     const [selectedEarthquake, setSelectedEarthquake] = useState<Earthquake | null>(null);
-
-
-//     const fetchEarthquakeData = async () => {
-//         const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2025-01-01&minmagnitude=2.5";
-//         const response = await fetch(url);
-
-//         if (!response.ok) {
-//             throw new Error("Failed to fetch earthquake data");
-//         }
-
-//         return response.json();
-//     }
-
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             try {
-//                 const data = await fetchEarthquakeData();
-
-//                 setEarthquakes(data.features);
-//                 // setEarthquakes(data);
-//             } catch (error) {
-//                 console.error("Error fetching earthquake data:", error);
-//             }
-//         };
-
-//         fetchData();
-//     }, []);
-
-//     if (!mapboxToken) {
-//         console.error("Mapbox token is missing. Check your environment variables.");
-//         return (
-//             <div className="text-red-500 text-center mt-10">
-//                 Error: Mapbox token not found.
-//             </div>
-//         );
-//     }
-
-//     return (
-//         <div className="flex flex-row w-full h-full max-h-full gap-x-4">
-//             {/* Show the Skeleton or the Map */}
-//             {loading && <Skeleton message="Loading earthquakes map..." />}
-
-//             <Map
-//                 initialViewState={initialViewState}
-//                 minZoom={2}
-//                 mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
-//                 mapboxAccessToken={mapboxToken}
-//                 ref={mapRef}
-//                 style={{ borderRadius: 8 }}
-//                 onLoad={() => setLoading(false)}
-//             >
-//                 {earthquakes.map((quake) => (
-//                     <Marker
-//                         key={quake.id}
-//                         longitude={quake.geometry.coordinates[0]}
-//                         latitude={quake.geometry.coordinates[1]}
-//                     >
-//                         <div
-//                             style={{
-//                                 width: "10px",
-//                                 height: "10px",
-//                                 backgroundColor: "red", // Replace with dynamic color if needed
-//                                 borderRadius: "50%",
-//                                 border: "1px solid white",
-//                             }}
-//                         ></div>
-//                     </Marker>
-//                 ))}
-
-//                 {/* Popup */}
-//                 {selectedEarthquake && (
-//                     <Popup
-//                         longitude={selectedEarthquake.geometry.coordinates[0]}
-//                         latitude={selectedEarthquake.geometry.coordinates[1]}
-//                         onClose={() => setSelectedEarthquake(null)}
-//                         closeOnClick={false}
-//                     >
-//                         <div>
-//                             <h3>{selectedEarthquake.properties.place}</h3>
-//                             <p>Magnitude: {selectedEarthquake.properties.mag}</p>
-//                             <p>Depth: {selectedEarthquake.geometry.coordinates[2]} km</p>
-//                         </div>
-//                     </Popup>
-//                 )}
-
-//                 {/* Native Mapbox controls */}
-//                 <ScaleControl position={"bottom-right"} />
-//                 <GeolocateControl position={"bottom-right"} />
-//                 <NavigationControl position={"bottom-right"} />
-
-//                 {/* Custom controls. */}
-//                 <div className="absolute top-2 left-0 right-0 hidden lg:block">
-//                     <ZoomLevelDisplay
-//                         mapRef={mapRef}
-//                         initialViewState={initialViewState}
-//                     />
-//                 </div>
-//             </Map>
-//         </div>
-//     );
-// };
-
-// export default EarthquakesMap;
--- src/app/mapbox/earthquakes/_earthquake-components/control-panel.tsx
+++ src/app/mapbox/earthquakes/_earthquake-components/control-panel.tsx
@@ -1,84 +1,87 @@
 import Link from "next/link";
 
+
 type ControlPanelProps = {
-  startTime: number; // Timestamp in milliseconds
-  endTime: number; // Timestamp in milliseconds
-  onChangeTime: (time: number) => void;
-  allDays: boolean;
-  onChangeAllDays: (value: boolean) => void;
-  selectedTime: number; // Timestamp in milliseconds
+	startTime: number; // Timestamp in milliseconds
+	endTime: number; // Timestamp in milliseconds
+	onChangeTime: (time: number) => void;
+	allDays: boolean;
+	onChangeAllDays: (value: boolean) => void;
+	selectedTime: number; // Timestamp in milliseconds
 };
 
 const formatTime = (time: number): string => {
-  const date = new Date(time);
-  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+	const date = new Date(time);
+	return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
 };
 
 export default function ControlPanel({
-  startTime,
-  endTime,
-  onChangeTime,
-  allDays,
-  onChangeAllDays,
-  selectedTime,
+	startTime,
+	endTime,
+	onChangeTime,
+	allDays,
+	onChangeAllDays,
+	selectedTime,
 }: ControlPanelProps) {
-  const day = 24 * 60 * 60 * 1000; // One day in milliseconds
-  const days = Math.round((endTime - startTime) / day); // Total number of days
-  const selectedDay = Math.round((selectedTime - startTime) / day); // Current selected day
+	const day = 24 * 60 * 60 * 1000; // One day in milliseconds
+	const days = Math.round((endTime - startTime) / day); // Total number of days
+	const selectedDay = Math.round((selectedTime - startTime) / day); // Current selected day
 
-  const onSelectDay = (evt: React.ChangeEvent<HTMLInputElement>): void => {
-    const daysToAdd = parseInt(evt.target.value, 10);
-    const newTime = startTime + daysToAdd * day;
-    onChangeTime(newTime);
-  };
+	const onSelectDay = (evt: React.ChangeEvent<HTMLInputElement>): void => {
+		const daysToAdd = parseInt(evt.target.value, 10);
+		const newTime = startTime + daysToAdd * day;
+		onChangeTime(newTime);
+	};
 
-  return (
-    <div className="bg-slate-300 text-black w-84 rounded-lg p-4">
-      <h3>Earthquakes</h3>
+	return (
+		<div className="bg-slate-300 text-black w-84 rounded-lg p-4">
+			<h3 className="text-2xl font-bold mb-1">Earthquakes:</h3>
 
-      <p className="text-black mb-2">
-        Map showing earthquakes from
-        <br />
-        <b>{formatTime(startTime)}</b> to <b>{formatTime(endTime)}</b>.
-      </p>
+			<p className="text-black text-lg mb-2">
+				Map showing earthquakes from
+				<br />
+				<b>{formatTime(startTime)}</b> to <b>{formatTime(endTime)}</b>.
+			</p>
 
-      <hr className="border-black border-2 my-2" />
+			<hr className="border-black border-2 my-2" />
 
-      <div>
-        <label>All Days</label>
-        <input
-          type="checkbox"
-          name="allday"
-          checked={allDays}
-          onChange={(evt) => onChangeAllDays(evt.target.checked)}
-        />
-      </div>
+			<div className="flex items-center gap-x-4">
+				<label>All Days</label>
 
-      <div className={`flex items-center ${allDays ? "opacity-50" : ""}`}>
-        <label className="w-48">
-          Each Day: {formatTime(selectedTime)}
-        </label>
-        <input
-          type="range"
-          disabled={allDays}
-          min={1}
-          max={days}
-          value={selectedDay}
-          step={1}
-          onChange={onSelectDay}
-        />
-      </div>
+				<input
+					type="checkbox"
+					name="allday"
+					checked={allDays}
+					onChange={(evt) => onChangeAllDays(evt.target.checked)}
+				/>
+			</div>
 
-      <hr className="border-black border-2 my-2" />
+			<div className={`flex items-center ${allDays ? "opacity-50" : ""}`}>
+				<label className="w-48">
+					Each Day: {formatTime(selectedTime)}
+				</label>
 
-      <p className="text-black mt-2">
-        Data source:{" "}
-        <Link href="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson">
-          earthquakes.geojson
-        </Link>
-      </p>
-    </div>
-  );
+				<input
+					type="range"
+					disabled={allDays}
+					min={1}
+					max={days}
+					value={selectedDay}
+					step={1}
+					onChange={onSelectDay}
+				/>
+			</div>
+
+			<hr className="border-black border-2 my-2" />
+
+			<p className="text-black mt-2">
+				Data source:{" "}
+				<Link href="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson">
+					earthquakes.geojson
+				</Link>
+			</p>
+		</div>
+	);
 }
 
 
